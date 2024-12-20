4b6fdd6df61c86f0b30166c5c3b698209da1a15f
madhat5 <davidmalary@gmail.com>
2024-12-20 12:33:56 -0500

    styles: import google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        garamond: ['EB Garamond', 'serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
