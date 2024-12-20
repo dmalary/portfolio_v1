a572d70e47cc6f0b6250509ebe50e5cecc69e718
madhat5 <davidmalary@gmail.com>
2024-12-19 19:23:07 -0500

    fix: update paths

--- src/App.jsx
+++ src/App.jsx
@@ -32,12 +32,12 @@ function App() {
           <div className='pt-2 pb-3 px-0'>{copyLang}</div>
           <hr />
           <div className='pt-2 pb-3 px-0'>
-            <div className='w-10 h-10 px-1 inline-block'><a href="https://github.com/dmalary" target="_blank" rel="noopener noreferrer"><img src="/portfolio_v1/imgs/github-v2.webp" alt="GitHub" /></a></div>
-            <div className='w-10 h-10 px-1 inline-block'><a href="https://www.linkedin.com/in/david-malary-95565a94" target="_blank" rel="noopener noreferrer"><img src="/portfolio_v1/imgs/sm-icons-linkedin-in-logo.webp" alt="LinkedIn" /></a></div>
-            <div className='w-10 h-10 px-1 inline-block'><a href="https://observablehq.com/@madhat5" target="_blank" rel="noopener noreferrer"><img src="/portfolio_v1/imgs/puzzle.png" alt="ObservableHQ" /></a></div>
-            <div className='w-10 h-10 px-1 inline-block'><a href="https://medium.com/@davidmalary" target="_blank" rel="noopener noreferrer"><img src="/portfolio_v1/imgs/puzzle.png" alt="Medium" /></a></div>
-            {/* <div className='w-10 h-10 px-1 inline-block'><a href="https://www.instagram.com/from_studio_jane/" target="_blank" rel="noopener noreferrer"><img src="/portfolio_v1/imgs/sm-icons-instagram-glyph-logo.webp" alt="Instagram" /></a></div> */}
-            {/* <div className='w-10 h-10 px-1 inline-block'><a href="https://dribbble.com/thedavmal" target="_blank" rel="noopener noreferrer"><img src="/portfolio_v1/imgs/puzzle.png" alt="Dribble" /></a></div> */}
+            <div className='w-10 h-10 px-1 inline-block'><a href="https://github.com/dmalary" target="_blank" rel="noopener noreferrer"><img src="/imgs/github-v2.webp" alt="GitHub" /></a></div>
+            <div className='w-10 h-10 px-1 inline-block'><a href="https://www.linkedin.com/in/david-malary-95565a94" target="_blank" rel="noopener noreferrer"><img src="/imgs/sm-icons-linkedin-in-logo.webp" alt="LinkedIn" /></a></div>
+            <div className='w-10 h-10 px-1 inline-block'><a href="https://observablehq.com/@madhat5" target="_blank" rel="noopener noreferrer"><img src="/imgs/puzzle.png" alt="ObservableHQ" /></a></div>
+            <div className='w-10 h-10 px-1 inline-block'><a href="https://medium.com/@davidmalary" target="_blank" rel="noopener noreferrer"><img src="/imgs/puzzle.png" alt="Medium" /></a></div>
+            {/* <div className='w-10 h-10 px-1 inline-block'><a href="https://www.instagram.com/from_studio_jane/" target="_blank" rel="noopener noreferrer"><img src="/imgs/sm-icons-instagram-glyph-logo.webp" alt="Instagram" /></a></div> */}
+            {/* <div className='w-10 h-10 px-1 inline-block'><a href="https://dribbble.com/thedavmal" target="_blank" rel="noopener noreferrer"><img src="/imgs/puzzle.png" alt="Dribble" /></a></div> */}
             {/* <hr /> */}
             {/* {add email/cv  icons} */}
           </div>
@@ -45,25 +45,25 @@ function App() {
         <div className='portfolio-col col-span-1 md:col-span-2 px-1 py-4'>
           <div className='section-card section-dev py-4'>
             <div className='section-titles text-left py-2'><h2>Development</h2></div>
-            <ProjectTile img={'/portfolio_v1/imgs/dev/thesis.png'} copy={"(2020) Thesis, MS in Data Visualization. Parson's School of Design, The New School"} link={'https://parsons.nyc/thesis-2021/'} size={'w-full py-3 md:w-5/6'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/dev/nfl_draft-project1.png'} copy={"An analysis of NFL draft positional history. Data gathered with custom built JS web scraper. Chart built with D3, layout in React."} link={'https://dmalary.github.io/nfl_draft-dashboard/'} size={'w-full py-3 md:w-5/6'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/dev/city_champions.png'} copy={"Small multiples donut chart, to evaluate what North American City has won the most champpionships, within the four major sports (baseball, football, basketball, hockey)."} link={'https://github.com/dmalary/react_d3-map_challenge_4'} size={'w-full py-3 md:w-5/6'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/dev/dm_visual_cv.png'} copy={"An upgraded version of my resume, based on a picture is worth a thousand words."} link={'https://github.com/dmalary/dm_cv-react-d3'} size={'w-full py-3 md:w-5/6'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/dev/state_teams.png'} copy={"Hexagon US map, showing the number of major 4 sports league teams by State. The major 4 are NFL, NHL, MLB, NBA."} link={'https://github.com/dmalary/react_d3-map_challenge_4?'} size={'w-full py-3 md:w-5/6'}/>
+            <ProjectTile img={'/imgs/dev/thesis.png'} copy={"(2020) Thesis, MS in Data Visualization. Parson's School of Design, The New School"} link={'https://parsons.nyc/thesis-2021/'} size={'w-full py-3 md:w-5/6'}/>
+            <ProjectTile img={'/imgs/dev/nfl_draft-project1.png'} copy={"An analysis of NFL draft positional history. Data gathered with custom built JS web scraper. Chart built with D3, layout in React."} link={'https://dmalary.github.io/nfl_draft-dashboard/'} size={'w-full py-3 md:w-5/6'}/>
+            <ProjectTile img={'/imgs/dev/city_champions.png'} copy={"Small multiples donut chart, to evaluate what North American City has won the most champpionships, within the four major sports (baseball, football, basketball, hockey)."} link={'https://github.com/dmalary/react_d3-map_challenge_4'} size={'w-full py-3 md:w-5/6'}/>
+            <ProjectTile img={'/imgs/dev/dm_visual_cv.png'} copy={"An upgraded version of my resume, based on a picture is worth a thousand words."} link={'https://github.com/dmalary/dm_cv-react-d3'} size={'w-full py-3 md:w-5/6'}/>
+            <ProjectTile img={'/imgs/dev/state_teams.png'} copy={"Hexagon US map, showing the number of major 4 sports league teams by State. The major 4 are NFL, NHL, MLB, NBA."} link={'https://github.com/dmalary/react_d3-map_challenge_4?'} size={'w-full py-3 md:w-5/6'}/>
           </div>
           <hr />
           <div className='section-card section-des py-4'>
             <div className='section-titles text-left py-2'><h2>Design</h2></div>
-            <ProjectTile img={'/portfolio_v1/imgs/des/focus_bottleneck.png'} size={'w-full md:w-full'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/des/bottle_and_glass-005.png'} size={'w-full md:w-2/3'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/des/two_bottles-005.png'} size={'w-full md:w-1/3'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/des/two_bottles-004.png'} size={'w-full md:w-1/2'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/des/bottle_and_glass-002.png'} size={'w-full md:w-1/2'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/des/glass_3point_light.png'} size={'w-full md:w-3/4'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/des/all_caps0001.png'} size={'w-full md:w-full'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/des/blue-0001.png'} size={'w-full md:w-3/5'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/des/kore_0001.png'} size={'w-full md:w-2/5'}/>
-            <ProjectTile img={'/portfolio_v1/imgs/des/studio_project-brochure.png'} size={'w-full md:w-full'}/>
+            <ProjectTile img={'/imgs/des/focus_bottleneck.png'} size={'w-full md:w-full'}/>
+            <ProjectTile img={'/imgs/des/bottle_and_glass-005.png'} size={'w-full md:w-2/3'}/>
+            <ProjectTile img={'/imgs/des/two_bottles-005.png'} size={'w-full md:w-1/3'}/>
+            <ProjectTile img={'/imgs/des/two_bottles-004.png'} size={'w-full md:w-1/2'}/>
+            <ProjectTile img={'/imgs/des/bottle_and_glass-002.png'} size={'w-full md:w-1/2'}/>
+            <ProjectTile img={'/imgs/des/glass_3point_light.png'} size={'w-full md:w-3/4'}/>
+            <ProjectTile img={'/imgs/des/all_caps0001.png'} size={'w-full md:w-full'}/>
+            <ProjectTile img={'/imgs/des/blue-0001.png'} size={'w-full md:w-3/5'}/>
+            <ProjectTile img={'/imgs/des/kore_0001.png'} size={'w-full md:w-2/5'}/>
+            <ProjectTile img={'/imgs/des/studio_project-brochure.png'} size={'w-full md:w-full'}/>
             {/* add donuts video, add pen commercial vid */}
           </div>
           <hr />
